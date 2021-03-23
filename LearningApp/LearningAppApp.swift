f40695500e48a0aa7145fbdd1fad8874e0745513
Jim LeBeau <jrlebeau@yahoo.com>
2021-03-23 13:58:38 -0700

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by James Lebeau on 3/23/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
